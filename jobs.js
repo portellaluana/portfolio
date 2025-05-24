eff221dc10c11a772d75b0928f58ffae4528c26e
portellaluana <luanaportella@gmail.com>
2025-05-24 15:49:34 -0300

    feat(portfolio): atualização de trabalhos e separação de código

---
+++ jobs.js
@@ -0,0 +1,189 @@
+export const jobs = [
+  {
+    name: "Shipping<br>quote<br>simulation",
+    ferramentas: ["VueJs | Figma"],
+    links: [
+      {
+        href: "https://github.com/portellaluana/shippingQuote",
+        img: "./assets/new-icon-light.png",
+      },
+    ],
+    background: "./assets/portfolio-shipping-quote.png",
+  },
+  {
+    name: "Agenda<br>Health<br>Care",
+    ferramentas: ["React | Typescript | Figma"],
+    links: [
+      {
+        href: "https://github.com/portellaluana/healthCare",
+        img: "./assets/new-icon-light.png",
+      },
+    ],
+    background: "./assets/portfolio-health-care.png",
+  },
+  {
+    name: "bolona<br>sorveteria<br>(mobile)",
+    ferramentas: ["Typescript | React | Figma"],
+    links: [
+      {
+        href: "https://github.com/portellaluana/bolona-web-app",
+        img: "./assets/new-icon-light.png",
+      },
+    ],
+    background: "./assets/portfolio-bolona.png",
+  },
+  {
+    name: "todo<br>kanban<br>dashboard",
+    ferramentas: ["React | js | Figma"],
+    links: [
+      {
+        href: "https://portellaluana.github.io/kanban-dashboard/",
+        img: "./assets/new-icon-light.png",
+      },
+    ],
+    background: "./assets/portfolio-kanban-dashboard.png",
+  },
+  {
+    name: "slot<br>Game<br>(mobile)",
+    ferramentas: ["Javascript"],
+    links: [
+      {
+        href: "https://portellaluana.github.io/arthur-game/",
+        img: "./assets/new-icon-light.png",
+      },
+    ],
+    background: "./assets/portfolio-slot-arthur.png",
+  },
+  {
+    name: "home<br>bakery",
+    ferramentas: ["React js"],
+    links: [
+      {
+        href: "https://portellaluana.github.io/react-minicart/",
+        img: "./assets/new-icon-light.png",
+      },
+    ],
+    background: "./assets/portfolio-gallery-bakery.png",
+  },
+  {
+    name: "foreo",
+    ferramentas: ["html"],
+    links: [
+      {
+        href: "https://portellaluana.github.io/foreo/",
+        img: "./assets/new-icon-light.png",
+      },
+    ],
+    background: "./assets/portfolio-gallery-foreo.png",
+  },
+  {
+    name: "check<br>speech<br>ai",
+    ferramentas: ["html | css | js | figma"],
+    links: [
+      {
+        href: "https://portellaluana.github.io/CheckSpeech-AI/",
+        img: "./assets/new-icon-light.png",
+      },
+    ],
+    background: "./assets/portfolio-gallery-checkspeechai.png",
+  },
+  {
+    name: "run<br>mario<br>run",
+    ferramentas: ["html | css | js | figma"],
+    links: [
+      {
+        href: "https://portellaluana.github.io/RunMarioRun/",
+        img: "./assets/new-icon-light.png",
+      },
+    ],
+    background: "./assets/portfolio-gallery-runmariorun.png",
+  },
+  {
+    name: "frutali",
+    ferramentas: ["xd | photoshop"],
+    links: [
+      {
+        href: "https://www.behance.net/gallery/97630845/Design-de-Interfaces-com-foco-em-ux",
+        img: "./assets/behance-icon-light.png",
+      },
+    ],
+    background: "./assets/portfolio-gallery-frutali.png",
+  },
+  {
+    name: "astrobot<br>mobile<br>concept",
+    ferramentas: ["photoshop | indesign"],
+    links: [
+      {
+        href: "https://www.behance.net/gallery/125344383/Astro-Bot-Mobile-Concept",
+        img: "./assets/behance-icon-light.png",
+      },
+    ],
+    background: "./assets/portfolio-gallery-astrobot.png",
+  },
+  {
+    name: "editech<br>summit",
+    ferramentas: ["photoshop | indesign"],
+    links: [
+      {
+        href: "https://www.behance.net/gallery/115237535/edTech-summit",
+        img: "./assets/behance-icon-light.png",
+      },
+    ],
+    background: "./assets/portfolio-gallery-editech.png",
+  },
+  {
+    name: "miditec<br>acate",
+    ferramentas: ["photoshop | indesign"],
+    links: [
+      {
+        href: "https://www.behance.net/gallery/89334891/MIDITEC-AWARDS-2019",
+        img: "./assets/behance-icon-light.png",
+      },
+    ],
+    background: "./assets/portfolio-gallery-miditec.png",
+  },
+  {
+    name: "startlab<br>acate",
+    ferramentas: ["photoshop | indesign"],
+    links: [
+      {
+        href: "https://www.behance.net/gallery/89268957/StartLab-ACATE",
+        img: "./assets/behance-icon-light.png",
+      },
+    ],
+    background: "./assets/portfolio-gallery-startlab.png",
+  },
+  {
+    name: "acate",
+    ferramentas: ["photoshop | indesign"],
+    links: [
+      {
+        href: "https://www.behance.net/gallery/97623727/Relatorio-de-atividades-ACATE",
+        img: "./assets/behance-icon-light.png",
+      },
+    ],
+    background: "./assets/portfolio-gallery-acate.png",
+  },
+  {
+    name: "rede<br>inovação<br>floripa",
+    ferramentas: ["photoshop | indesign"],
+    links: [
+      {
+        href: "https://www.behance.net/gallery/77956463/Rede-de-Inovacao",
+        img: "./assets/behance-icon-light.png",
+      },
+    ],
+    background: "./assets/portfolio-gallery-redeinocacao.png",
+  },
+  {
+    name: "hempness",
+    ferramentas: ["photoshop | indesign"],
+    links: [
+      {
+        href: "https://www.behance.net/gallery/56335477/Hempness-Camisetas",
+        img: "./assets/behance-icon-light.png",
+      },
+    ],
+    background: "./assets/portfolio-gallery-hempness.png",
+  },
+];
